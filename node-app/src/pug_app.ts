bac43222d94703f65dbdb9641c9f6d8aa9f15fd5
itc-s24016 <s24016@std.it-college.ac.jp>
2025-10-07 16:22:19 +0900

    ルーティングまで

--- node-app/src/pug_app.ts
+++ node-app/src/pug_app.ts
@@ -1,18 +1,39 @@
 import http from "node:http"
 import pug from 'pug'
+import url from 'node:url'
+import fs from 'node:fs/promises'
 
-// pugで記述したものをHTMLに変換して表示する
+// 最初で読み込んで置くことで、毎回読み込む必要がなくなる
+const index_template = pug.compileFile('./index.pug')
+const style_css = await fs.readFile('./style.css', 'utf-8')
 
 const server = http.createServer(getFromClient)
-
 server.listen(3210)
 
 async function getFromClient(req: http.IncomingMessage, res: http.ServerResponse) {
-    const content = pug.renderFile('./index.pug', {
-        title: 'Index',
-        content: 'これはPugを使ったWebページです。'
-    })
-    res.writeHead(200, {'Content-Type': 'text/html; charset=utf-8'})
-    res.write(content)
-    res.end()
+    // 第二引数に適当な URL を指定すると、req.url が undefined でもエラーにならない
+    const url_parts = new url.URL(req.url || '', 'http://localhost:3210')
+
+    switch (url_parts.pathname) {
+        case '/':
+            const content = index_template({
+                title: 'Index',
+                content: 'これはテンプレートを使ったサンプルページです。'
+            })
+            res.writeHead(200, {'Content-Type': 'text/html; charset=utf-8'})
+            res.write(content)
+            res.end()
+            break
+
+        case '/style.css':
+            res.writeHead(200, {'Content-Type': 'text/css; charset=utf-8'})
+            res.write(style_css)
+            res.end()
+            break
+
+        default:
+            res.writeHead(404, {'Content-Type': 'text/plain'})
+            res.end('no page...')
+            break
+    }
 }
