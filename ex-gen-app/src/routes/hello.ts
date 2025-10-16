e36314291024208c6517fb4fae76d598b7619772
itc-s24016 <s24016@std.it-college.ac.jp>
2025-10-16 16:34:35 +0900

    フォームの送信まで

--- ex-gen-app/src/routes/hello.ts
+++ ex-gen-app/src/routes/hello.ts
@@ -4,13 +4,21 @@ const router = Router();
 
 // /hello ではなく、 / にすることで、 app.ts で /hello を指定した際に正しく動作する
 router.get('/', (req, res, next) => {
-    const name = req.query.name
-    const mail = req.query.mail
     const data = {
         title: 'Hello',
-        content: `あなたの名前は ${name} です。<br> メールアドレスは ${mail} です。`
+        content: '※何か書いて送信してください'
     }
     res.render('hello', data);
+})
+
+router.post('/post', async (req, res, next) => {
+    // string | undefined にすることにより、1 + 1 を計算してしまうバグを防ぐ
+    const msg = req.body.message as string | undefined
+    const data ={
+        title: 'Hello!',
+        content: `あなたは、「${msg}」と送信しました。`
+    }
+    res.render('hello', data)
 })
 
 // この行で import できるようにする
