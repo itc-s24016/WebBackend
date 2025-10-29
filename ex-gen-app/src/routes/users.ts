9fc4c025d1c4a7eb00f6de8a2c708f44976cceac
itc-s24016 <s24016@std.it-college.ac.jp>
2025-10-29 16:57:17 +0900

    レコード追加ページのルーティングまで

--- ex-gen-app/src/routes/users.ts
+++ ex-gen-app/src/routes/users.ts
@@ -52,4 +52,10 @@ router.get('/find', async (req: Request<{}, {}, {}, UserParams>, res, next) => {
     })
 })
 
+router.get('/add', async (req, res, next) => {
+    res.render('users/add', {
+        title: 'Users/Add',
+    })
+})
+
 export default router
