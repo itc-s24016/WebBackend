bb9ec82c17613840d1eb751ac418a9938fda28b0
itc-s24016 <s24016@std.it-college.ac.jp>
2025-10-29 16:39:33 +0900

    ID で検索まで

--- ex-gen-app/src/routes/users.ts
+++ ex-gen-app/src/routes/users.ts
@@ -1,10 +1,55 @@
-import {Router} from 'express'
+import {Request, Router} from 'express'
+import {PrismaMariaDb} from '@prisma/adapter-mariadb'
+import { PrismaClient } from 'db'
 
 const router = Router()
 
-/* Get users listing. */
-router.get('/', async (req, res, next) => {
-    res.send('respond with a resource')
+const adapter = new PrismaMariaDb({
+    host: 'localhost',
+    port: 3306,
+    user: 'prisma',
+    password: 'prisma',
+    database: 'chap6',
+    connectionLimit: 5
+})
+
+const prisma = new PrismaClient({adapter})
+
+interface UserParams {
+    id?: string
+    name?: string
+    min?: string
+    max?: string
+    mail?: string
+}
+
+router.get('/', async (req: Request<{}, {}, {}, UserParams>, res, next) => {
+    const id = parseInt(req.query.id || '')
+
+    const users = await (id ? prisma.user.findMany({where: {id}})
+        : prisma.user.findMany())
+
+    res.render('users/index', {
+        title: 'Users/Index',
+        content: users,
+    })
+})
+
+// 複数検索ができるようになった
+router.get('/find', async (req: Request<{}, {}, {}, UserParams>, res, next) => {
+    const {name, mail} = req.query
+
+    const users = await prisma.user.findMany({where: {
+        OR: [
+            {name: {contains: name}},
+            {mail: {contains: mail}},
+        ]
+    }})
+
+    res.render('users/index', {
+        title: 'Users/Find',
+        content: users,
+    })
 })
 
 export default router
